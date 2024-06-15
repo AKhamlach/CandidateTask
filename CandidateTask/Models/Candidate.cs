ca2af5d8b4d368ab9ad4f46dc02697204132eb70
AKhamlach <khamlach.anas1@gmail.com>
2024-06-15 15:31:10 +0100

    Candidate Model Added

---
+++ CandidateTask/Models/Candidate.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateTask.Models
+{
+    public class Candidate
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public required string FirstName { get; set; }
+        [Required]
+        public required string LastName { get; set; }
+        [RegularExpression(@"^[0-9]*$")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        public string? PreferedCallInterval { get; set; }
+        [Url]
+        public string? LinkedInProfile { get; set; }
+        [Url]
+        public string? GitHubProfile { get; set; }
+        [Required]
+        public required string Comment { get; set; }
+    }
+}
